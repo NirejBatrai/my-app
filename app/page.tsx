93b93c6e2a11831ebdb7a51eab0e43bf515008a8
Nil uzuma <bikerentphuket@gmail.com>
2025-04-03 10:54:52 +0700

    added color in p tag and added border

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
 export default function WebApp() {
   return (
-    <>
-      <div>
-        <h1>Hello world</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi,
-          deleniti.
-        </p>
-      </div>
-    </>
+    <div className="w-[80%] border border-white rounded p-4">
+      <h1 className="text-5xl font-bold m-2 p-2 border border-black rounded">
+        Hello world
+      </h1>
+      <p className=" m-2 p-2 bg-gray-500/60 text-black border-black rounded">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, magnam.
+      </p>
+      <p className=" m-2 p-2 bg-green-500/60 text-black border-black rounded">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, magnam.
+      </p>
+      <p className="m-2 p-2 bg-yellow-500/60 text-black border-black rounded">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, magnam.
+      </p>
+      <p className="m-2  p-2 bg-orange-500/60 text-black border-black rounded">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+        numquam saepe, eveniet perferendis odio voluptatem sapiente
+        necessitatibus optio illum laudantium laboriosam dolorum eum eius
+        provident vitae ea magnam blanditiis ab aliquid doloremque commodi minus
+        quia pariatur quod. Dignissimos quibusdam laboriosam deleniti, deserunt
+        reprehenderit debitis nobis qui quod possimus, sapiente ab blanditiis
+        hic. Mollitia ipsum atque in earum ab? At rem nulla voluptatum
+        consequatur a nesciunt reprehenderit suscipit distinctio temporibus id
+        eius sit vero, in repellendus illum, enim quae eos dignissimos unde
+        porro assumenda perspiciatis ad ullam. Inventore deserunt placeat
+        officiis, voluptate, vero omnis mollitia neque et quos nesciunt facere
+        minima!{" "}
+      </p>
+    </div>
   );
 }
