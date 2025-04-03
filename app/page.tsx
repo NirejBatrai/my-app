b6c484c1621ed0f5a1fb7eeca7caba2a3dae2f3c
Nil uzuma <bikerentphuket@gmail.com>
2025-04-03 10:25:27 +0700

    add h1 and p tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function WebApp() {
-  return <div>Hello Brotherrrr</div>;
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi,
+          deleniti.
+        </p>
+      </div>
+    </>
+  );
 }
