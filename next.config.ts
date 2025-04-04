02eb59c2cc0d074c74ca82cc30fcefc5d2a4b0c0
Nil uzuma <bikerentphuket@gmail.com>
2025-04-04 09:06:29 +0700

    addded images and banner

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "computing.psu.ac.th", pathname: "/**" },
+    ],
+  },
   /* config options here */
 };
 
